baab05fe774c34301c7edb7c05e65902682b8dce
mihir-codes <mihirsinh.parmar.it@gmail.com>
2020-05-20 00:15:59 +0530

    modify in single buildpack

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var AssetManager = artifacts.require("./AssetManager.sol");
-var AuctionManager = artifacts.require("./AuctionManager.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(AssetManager);
-  deployer.deploy(AuctionManager)
-};
